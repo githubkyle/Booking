b6d0ab391036b553dbf07674f02a3121a2ad6210
kshwi <kshwiss@gmail.com>
2023-07-03 20:25:23 -0500

    worked on resolvers and typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,57 @@
-const { Schema, model } = require("mongoose");
+const { User, Book } = require("../models");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const SECRET = "mysecretsshhhhh";
+const resolvers = {
+  Query: {
+    getSingleUser: async () => {
+      return User.find({ username });
+    }
+  },
+  Mutation: {
+    createUser: async (parent, args) => {
+      const newUser = await User.create(args);
+      return "New User Created!";
+    },
+    saveBook: async (parent, { username, bookId }) => {
+      const savedBook = await User.findOneAndUpdate(
+        {
+          username
+        },
+        { $$addToSet: { savedBooks: bookId } },
+        { new: true }
+      );
+      return User.savedBooks;
+    },
+
+    deleteBook: async (parent, { username, bookId }) => {
+      const nukedBook = await User.findOneAndUpdate(
+        { username },
+        { $pull: { savedBooks: bookId } },
+        { new: true }
+      );
+      return User.savedBooks;
+    },
+    login: async (parent, { username, password }) => {
+      try {
+        const user = await User.findOne({ username });
+        if (!user) {
+          throw new Error("Unknown user...");
+        }
+        const passValid = await bcrypt.compare(password, user.password);
+        if (!passValid) {
+          throw new Error("Bad password");
+        }
+        const token = jwt.sign({ userId: user.id }, SECRET, {
+          expiresIn: "2h"
+        });
+        return { token, user };
+      } catch (error) {
+        console.error(error);
+        throw new Error("Error logging in");
+      }
+    }
+  }
+};
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,33 @@
-const { Schema, model } = require("mongoose");
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    username: ID!
+    email: String!
+    password: String!
+
+  }
+
+  type Book {
+    authors: String
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Query {
+    getSingleUser: User
+      }
+
+  type Mutation {
+    createUser(username: ID!, email:String!, password:String!): User
+    saveBook(username: ID!,bookId: String!)
+    deleteBook(username: ID!,bookId: String!)
+    login(username: ID!,password: String!)
+    
+  }
+`;
+
+module.exports = typeDefs;
